6502b4f148c327897012f86d61fac8044bd8a85d
Minttu Hurme <ammsalme@users.noreply.github.com>
2021-03-23 09:05:16 +0200

    Update dependecies and drone (#20)
    * Bump handlebars from 4.2.0 to 4.7.6
    
    Bumps [handlebars](https://github.com/wycats/handlebars.js) from 4.2.0 to 4.7.6.
    - [Release notes](https://github.com/wycats/handlebars.js/releases)
    - [Changelog](https://github.com/handlebars-lang/handlebars.js/blob/master/release-notes.md)
    - [Commits](https://github.com/wycats/handlebars.js/compare/v4.2.0...v4.7.6)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump yargs-parser from 13.1.1 to 13.1.2
    
    Bumps [yargs-parser](https://github.com/yargs/yargs-parser) from 13.1.1 to 13.1.2.
    - [Release notes](https://github.com/yargs/yargs-parser/releases)
    - [Changelog](https://github.com/yargs/yargs-parser/blob/master/docs/CHANGELOG-full.md)
    - [Commits](https://github.com/yargs/yargs-parser/commits)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump ini from 1.3.5 to 1.3.7
    
    Bumps [ini](https://github.com/isaacs/ini) from 1.3.5 to 1.3.7.
    - [Release notes](https://github.com/isaacs/ini/releases)
    - [Commits](https://github.com/isaacs/ini/compare/v1.3.5...v1.3.7)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump axios from 0.5.4 to 0.21.1
    
    Bumps [axios](https://github.com/axios/axios) from 0.5.4 to 0.21.1.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/v0.21.1/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.5.4...v0.21.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump xmldom from 0.1.19 to 0.5.0
    
    Bumps [xmldom](https://github.com/xmldom/xmldom) from 0.1.19 to 0.5.0.
    - [Release notes](https://github.com/xmldom/xmldom/releases)
    - [Changelog](https://github.com/xmldom/xmldom/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/xmldom/xmldom/commits/0.5.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update .drone.yml
    * Fix tests
    * Update (some) dependencies
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>

--- test/client.spec.js
+++ test/client.spec.js
@@ -26,6 +26,8 @@
 *
 **/
 
+const {MarcRecord} = require('@natlibfi/marc-record');
+
 /*jshint mocha:true*/
 
 (function(root, testrunner) {
@@ -37,146 +39,151 @@
       require('./config'),
       require('chai'), 
       require('../lib/index'),
-      require('marc-record-js'));  // jshint ignore:line
-    } 
+      require('@natlibfi/marc-record'));  // jshint ignore:line
+    }
   }(this, function(config, chai, MelindaClient, Record) {
     "use strict";
     var expect = chai.expect;
-    
+
     describe('Client', function() {
-      
+
       it('should be a constructor', function() {
-        
+
         expect(MelindaClient).to.be.a('function');
       });
-      
+
       it('should be instantiable', function() {
         var client = new MelindaClient(config);
-        
+
         expect(client).to.be.an('object');
       });
-      
+
       it('should be able to load a record', function(done) {
         this.timeout(5000);
-        
+
         var client = new MelindaClient(config);
-        
-        client.loadRecord(30000).then(function(record) {
-          
-          expect(record).to.be.an.instanceof(Record); 
-          
+
+        client.loadRecord("000503874").then(function(record) {
+
+          expect(record).to.be.an.instanceof(MarcRecord); 
+
           done();
         }).catch(function(error) {
           throw error;
         }).done();
       });
-      
+
       it('should throw an error if record is not found', function(done) {
         this.timeout(5000);
-        
+
         var client = new MelindaClient(config);
-        
+
         client.loadRecord("9999/9999999").then(function(record) {
           throw new Error("Loaded invalid record succesfully!");
         }).catch(function(error) {
-          
+
           if (error.name == "AssertionError") {
             throw error;
           }
-          
-          expect(error.status_code).to.equal(404);
-          expect(error.message).to.equal('Not Found');
+          //console.log("code: "+error.response.status);
+          //console.log("message: "+error.response.statusText);
+
+          expect(error.response.status).to.equal(404);
+          expect(error.response.statusText).to.equal('Not Found');
           done();
         }).done();
-        
+
       });
-      
+
       it('should be able to update a record', function(done) {
         this.timeout(7000);
         var client = new MelindaClient(config);
-        
-        client.loadRecord(30001).then(function(record) {
-          
-          expect(record).to.be.an.instanceof(Record);
-          
+
+        client.loadRecord("016525548").then(function(record) {
+
+          // console.log(record.constructor.name)
+          expect(record).to.be.an.instanceof(MarcRecord);
+
           //remove old test lines
           record.fields = record.fields.filter(function(field) {
             return !(field.tag == "599" && field.subfields.map(toValues).indexOf("TEST") > -1);
           });
-          
+
           //create new test lines
           var now = new Date();
           record.insertField( ["599",'','','a','TEST','c', now.toString()] );
-          record.insertField( ["599",'','','a','TEST <TAG>'] );
-          
+          // record.insertField( ["599",'','','a','TEST <TAG>'] );
+
           client.updateRecord(record).then(function(response) {
-            
-            expect(response.messages.map(to('message'))).to.contain('Document: 000030001 was updated successfully.');
+
+            // console.log(response);
+            expect(response.messages.map(to('message'))).to.contain('Document: 016525548 was updated successfully.');
             expect(response.messages.map(to('code'))).to.contain('0018');
-            
-            
+
             //validate that the update was ok
-            client.loadRecord(30001).then(function(record) {
-              
+            client.loadRecord("016525548").then(function(record) {
+
               var testChange = record.fields.filter(function(field) {
                 return (field.tag == "599" && field.subfields.map(toValues).indexOf("TEST") > -1);
               });
               expect(testChange).length.to.be(1);
               expect(testChange[0].subfields.map(toValues)).to.contain(now.toString());
-              
+
               done();
             }).done();
-            
+
           }).catch(function(e) {
             throw e;
           }).done();
-          
+
         }).catch(function(error) {
           throw error;
         }).done();
-        
+
       });
-      
+
       it('should be able to create a new record', function(done) {
         this.timeout(7000);
         var client = new MelindaClient(config);
-        
-        client.loadRecord(30001).then(function(record) {
-          
+
+        //console.log("Reading record");
+        client.loadRecord("016525548").then(function(record) {
+
+          console.log("Creating record");
           client.createRecord(record).then(function(response) {
-            
-            expect(response.recordId).to.not.equal(30001);
+            console.log("Done.");
+            // console.log(response);
+            expect(response.recordId).to.not.equal("016525548");
             console.log(response.recordId);
             done();
           }).catch(function(e) {
             throw e;
           }).done();
-          
+
         }).catch(function(error) {
           throw error;
         }).done();
-        
+
       });
-      
+
       it('should retrieve child records', function(done) {
-        
-        this.timeout(5000);
-        
+
+        this.timeout(15000);
+
         var client = new MelindaClient(config);
-        
-        client.loadChildRecords('foo').then(function(records) {
-          
-          expect(records).to.be.an('array');
-          
+
+        client.loadChildRecords('014722671').then(function(records) {
+          console.log(`Received records: ${records.length}`);
+          expect(records.length).to.equal(13);
+          // console.log(records);
           done();
         }).catch(function(error) {
           throw error;
         }).done();
-        
+
       });
-      
     });
-    
+
     function to(attr) {
       return function(obj) {
         return obj[attr];
@@ -186,6 +193,4 @@
       return obj.value;
     }
   }));
-  
-  
-  
+
